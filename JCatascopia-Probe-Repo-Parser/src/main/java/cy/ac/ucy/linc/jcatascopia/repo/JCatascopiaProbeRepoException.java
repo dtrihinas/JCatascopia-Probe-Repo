e194a0fec6c2da0077e504dc9945581a9322dc38
dtrihinas <trihinas@cs.ucy.ac.cy>
2015-03-12 17:23:18 +0200

    JCatascopia Probe Repo Metadata Parser

---
+++ JCatascopia-Probe-Repo-Parser/src/main/java/cy/ac/ucy/linc/jcatascopia/repo/JCatascopiaProbeRepoException.java
@@ -0,0 +1,32 @@
+package cy.ac.ucy.linc.jcatascopia.repo;
+
+public class JCatascopiaProbeRepoException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	private String message = null;
+	 
+	public JCatascopiaProbeRepoException() {
+		super();
+	}
+	 
+	public JCatascopiaProbeRepoException (String message) {
+		super(message);
+	    this.message = "JCatascopia: " + message;
+	}
+	 
+	public JCatascopiaProbeRepoException(Throwable cause) {
+		super(cause);
+	}
+	
+	 
+	@Override
+	public String toString() {
+		return message;
+	}
+	 
+	@Override
+	public String getMessage() {
+		return message;
+	}
+}
